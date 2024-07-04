64d520db97f386587d25215ba4b058a3678bcf9c
Kaanyinaele <110630626+kaanyinaele@users.noreply.github.com>
2024-07-04 23:01:32 +0100

    app.py
    update host and port for Heroku

--- app.py
+++ app.py
@@ -112,4 +112,5 @@ def uploaded_file(filename):
         return handle_error(f'File {filename} not found')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT',5000))
+    app.run(host='0.0.0.0', port=port)
